9fd8b78e2da83490ed48f299950d2b22c4953db8
ThePolanco <polancojeison21@gmail.com>
2024-01-16 11:54:55 -0500

    Update

--- codigo.js
+++ codigo.js
@@ -1,5 +1,16 @@
+/*
 var numeros = 15;
 const nombre = "Jeison"; 
 let numero = 14;
 numero = 12;
 alert(numero);
+*/
+
+/*
+let nombre = prompt("Hola: ")
+alert("Hola " + nombre)
+*/
+
+numero = 12;
+numero += 5;
+document.write(numero)
