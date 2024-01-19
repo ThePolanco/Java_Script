65504c7a1be851013df5f6d8c269e4c2042f04df
ThePolanco <polancojeison21@gmail.com>
2024-01-18 19:08:34 -0500

    Metodos con cadenas

---
+++ metodos_cadenas.js
@@ -0,0 +1,12 @@
+let cadena = "cadena de prueba";
+let cadena2 = "cadena";
+
+//resultado = cadena.concat(cadena2);
+//resultado = cadena.startsWith(cadena2);
+//resultado = cadena.endsWith(cadena2)
+//resultado = cadena.includes(cadena2);
+//resultado = cadena.indexOf(cadena2);  
+
+document.write(resultado)
+
+//5:21:00
