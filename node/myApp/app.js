bc4fdbf9117cc679cb5ba509a36f54a6f1ca57bb
ThePolanco <polancojeison21@gmail.com>
2024-01-27 22:55:16 -0500

    Avances freeCodeCamp

---
+++ node/myApp/app.js
@@ -0,0 +1,13 @@
+// const { saludar, saludarHolaMundo } = require("./saludos.js");
+// console.log(saludar('Jeison'))
+// console.log(saludarHolaMundo());
+
+
+// console.log('Hola Mundo');
+// console.warn('Ocurrio un error');
+// console.error('ERROR!');
+// console.error(new Error('Algo Fallo!'));
+// console.log(process.env);
+// console.log(process.argv);
+// console.log(process.memoryUsage());
+//1:17:00
