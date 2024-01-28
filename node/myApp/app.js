845c5dc6c28cfcf932985a0f3b5f5f1d5cabb667
ThePolanco <polancojeison21@gmail.com>
2024-01-28 10:51:37 -0500

    Modulos node.js

--- node/myApp/app.js
+++ node/myApp/app.js
@@ -10,4 +10,71 @@
 // console.log(process.env);
 // console.log(process.argv);
 // console.log(process.memoryUsage());
-//1:17:00
+
+
+// const os = require('os');
+// console.log(os.type());
+// console.log(os.homedir());
+// console.log(os.uptime());
+// console.log(os.userInfo());
+
+
+// function mostrarTema(tema){
+//   console.log(`Estoy aprendiendo ${tema}`);
+// }
+// setTimeout(mostrarTema, 1000, 'Node.js');
+// function sumar(a, b){
+//   console.log(a + b);
+// }
+// setTimeout(sumar, 2000, 5, 5);
+
+
+// function mostrarTema(tema){
+//   console.log(`Estoy aprendiendo ${tema}`);
+// }
+// setImmediate(mostrarTema, 'Node.js');
+
+
+// function mostrarTema(tema){
+//   console.log(`Estoy aprendiendo ${tema}`);
+// }
+// setInterval(mostrarTema, 2000, 'Node.js');
+
+
+//fs: File System
+const fs = require('fs');
+
+// fs.readFile('index.html', 'utf-8', (err, contenido) => {
+//   if(err){
+//     throw err;
+//   }
+//   console.log(contenido);
+// });
+
+// fs.rename('index.html', 'main.html', (err) =>{
+//   if(err){
+//     throw err;
+//   }
+//   console.log('Modificación ejecutada correctamente.');
+// });
+
+// fs.appendFile('index.html', '<p>Hola</p>', (err) =>{
+//   if(err){
+//     throw err;
+//   }
+//   console.log('Archivo actualizado.')
+// });
+
+// fs.writeFile('index.html', 'Contenido Nuevo', (err) => {
+//   if(err){
+//     throw err;
+//   }
+//   console.log('Contenido reemplazado.');
+// });
+
+// fs.unlink('index.html', (err) =>{
+//   if (err){
+//     throw err;
+//   }
+//   console.log('Archivo eliminado')
+// });
